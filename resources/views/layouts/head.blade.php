c6e20be0d40c00d12569608de9d913fc7626ee4a
noel <toyonoel@gmail.com>
2017-10-04 15:25:52 -0400

    [ADD] agregue el layout

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,15 @@
+<head>
+	<title>@yield('content', 'default') | Title</title>
+
+	<link href="{{ asset('css/bootstrap.min.css')}}" rel="stylesheet">
+
+	<link href="{{ asset('fonts/css/font-awesome.min.css')}}" rel="stylesheet">
+	<link href="{{ asset('css/animate.min.css')}}" rel="stylesheet">
+
+	<link href="{{ asset('css/custom.css')}}" rel="stylesheet">
+
+	<script src="{{ asset('plugins/jquery.min.js')}}"></script>
+
+	
+	@yield('head')
+</head>
