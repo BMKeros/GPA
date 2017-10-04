f2de6e6c245527808e7b3c458634de44173cb92e
Boanerget <antonyprogramacion96@gmail.com>
2017-10-04 16:36:43 -0400

    [MOD] Agradas etiquetas meta.

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,9 +1,19 @@
 <head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+	<meta charset="utf-8">
+	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<title>@yield('content', 'default') | Title</title>
 
 	<link href="{{ asset('css/bootstrap.min.css')}}" rel="stylesheet">
 
 	<link href="{{ asset('fonts/css/font-awesome.min.css')}}" rel="stylesheet">
+	
 	<link href="{{ asset('css/animate.min.css')}}" rel="stylesheet">
 
 	<link href="{{ asset('css/custom.css')}}" rel="stylesheet">
