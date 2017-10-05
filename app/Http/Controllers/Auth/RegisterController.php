afcb05d2c95937d5eea0f8656ab35e1b77c55a88
AjxGnx <alirio1925@gmail.com>
2017-10-05 12:48:33 -0400

    [ADD][MOD] se coloco rol por defecto al crear user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -78,6 +78,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'rol_id'=> 2,
         ]);
 
 
