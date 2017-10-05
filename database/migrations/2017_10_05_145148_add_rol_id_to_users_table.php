7f573268e85202c47b1330cd6007a473acc25e37
AjxGnx <alirio1925@gmail.com>
2017-10-05 11:46:56 -0400

    [ADD] se agredo migracion para rol_id en users

---
+++ database/migrations/2017_10_05_145148_add_rol_id_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRolIdToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::enableForeignKeyConstraints();
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('rol_id')->unsigned()->after('password');
+            $table->foreign('rol_id')->references('id')->on('rols');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
