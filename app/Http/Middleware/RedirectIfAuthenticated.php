d008a18b96241b68d4052ed3597bf32a9178b8f6
Israel Lugo <hostelixisrael@gmail.com>
2017-10-05 17:54:20 -0400

    [MOD] cambiada la logica para la redireccion en login

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,10 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            if (Auth::user()->role->id == 1) {
+                return redirect('/admin/dashboard');
+            }
+            return redirect('/user/dashboard');
         }
 
         return $next($request);
