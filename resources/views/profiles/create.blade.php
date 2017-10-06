aeea0cca6c2c997b030000ed46dbf1e8561a474b
Israel Lugo <hostelixisrael@gmail.com>
2017-10-06 11:13:55 -0400

    [MOD] modificaciones en el nombre de los botones

--- resources/views/profiles/create.blade.php
+++ resources/views/profiles/create.blade.php
@@ -104,8 +104,8 @@
                         <div class="ln_solid"></div>
                         <div class="form-group">
                             <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-                                <button type="submit" class="btn btn-primary">Cancel</button>
-                                <button type="submit" class="btn btn-success">Submit</button>
+                                <button type="submit" class="btn btn-primary">Cancelar</button>
+                                <button type="submit" class="btn btn-success">Guardar</button>
                             </div>
                         </div>
 
