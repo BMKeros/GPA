9934fc6e9f5536b6425a7f72f7839b5f7c25d291
Israel Lugo <hostelixisrael@gmail.com>
2017-10-06 11:06:28 -0400

    [MOD] agregados nuevos usuarios el seeder de usuarios

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Eloquent\Model;
 use App\User;
 
 
@@ -14,14 +13,26 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        $data = array( 
+        $data = [
             [
                 'name' => 'admin',
-                'email' => 'admin@admin.com',
+                'email' => 'admin@bmkeros.org.ve',
                 'password' => bcrypt('admin'),
                 'role_id' => 1
-            ]
-        );
+            ],
+            [
+                'name' => 'user',
+                'email' => 'user@bmkeros.org.ve',
+                'password' => bcrypt('user'),
+                'role_id' => 2
+            ],
+            [
+                'name' => 'socio',
+                'email' => 'socio@bmkeros.org.ve',
+                'password' => bcrypt('socio'),
+                'role_id' => 3
+            ],
+        ];
 
         User::insert($data);
     }
