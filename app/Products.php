7b00b7c51c244144ddb07a082120ac39c746a5b2
noel <toyonoel@gmail.com>
2017-10-06 14:53:39 -0400

    [ADD] se agrego campo cantidad

--- app/Products.php
+++ app/Products.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Products extends Model
 {
-    protected $fillable = ['categories_id', 'units_id','name', 'slug', 'brand', 'description','price', 'associated_percentage', 'street_percentage', 'product_limit', 'quantity_available', 'image'];
+    protected $fillable = ['categories_id', 'units_id','name', 'slug', 'brand', 'description','price', 'associated_percentage', 'street_percentage', 'product_limit', 'quantity_available', 'quantity' , 'image'];
 }
