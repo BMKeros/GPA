e824fc9031fb19f575dafd50182aba4231c4cbbd
Israel Lugo <hostelixisrael@gmail.com>
2017-10-06 15:00:46 -0400

    [ADD][MOD] agregada la traduccion para lang es

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,17 +64,11 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-        $messages = [
-            'required' => 'El :attribute es requerido.',
-            'unique' => 'El :attribute ya existe.',
-            'confirmed' => 'El :attribute no coincide',
-        ];
-
         return Validator::make($data, [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
-        ], $messages);
+        ]);
     }
 
 
