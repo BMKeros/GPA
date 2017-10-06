5d926d6a7a12506da73b41b99cd12cae9b8c196c
noel <toyonoel@gmail.com>
2017-10-06 14:33:28 -0400

    [ADD] se agrego el modelo de productos

---
+++ app/Products.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    protected $fillable = ['categories_id', 'units_id','name', 'slug', 'brand', 'description','price', 'associated_percentage', 'street_percentage', 'product_limit', 'quantity_available', 'image'];
+}
