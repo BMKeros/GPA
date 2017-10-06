391892af517fad6ee60766fb96b112924cf357d5
Jesus Navas <jesusnavas2210@gmail.com>
2017-10-05 22:22:44 -0400

    [MOD] se modifico el nombre del seeder roles

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Role;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::insert([
+            [
+                'id' => 1,
+                'name' => 'ADMIN'
+            ],
+            [
+                'id' => 2,
+                'name' => 'USER'
+            ],
+            [
+                'id' => 3,
+                'name' => 'SOCIO'
+            ]
+        ]);
+    }
+}
