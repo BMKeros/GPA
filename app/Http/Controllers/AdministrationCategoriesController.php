4cf0f9f14bee3f381507a67859e69dffd1323da3
noel <toyonoel@gmail.com>
2017-10-07 00:26:57 -0400

    [ADD] se agrego el controlador para categoria y su crud

---
+++ app/Http/Controllers/AdministrationCategoriesController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Categories;
+
+
+class AdministrationCategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Categories::all();
+        return view('admin.categories.index', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $category = null;
+        return view('admin.categories.create', ['category' => $category]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       
+        $data = $request->all();
+
+        $categories = Categories::create([
+            'name' => $data['name'],
+            'description' => $data['description'],
+            'slug' => strtolower($data['name']),
+        ]);
+    
+        return redirect()->route('categories.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $category = Categories::findOrFail($id);
+
+        return view('admin.categories.create', ['category' => $category]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $category = Categories::findOrFail($id);
+        $category->name = $request->name;
+        $category->description = $request->description;
+        $category->slug = strtolower($request->description);
+        $category->save();
+
+        return redirect()->route('categories.index');
+
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $category = Categories::findOrFail($id);
+
+        $category->delete();
+
+        
+        return redirect()->route('categories.index');
+    }
+}
