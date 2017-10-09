215f84c6bf11f0af9c3ddbf9bf70378795acb511
danzt <danztty@gmail.com>
2017-10-09 17:54:47 -0400

    [MOD] se cambio la indentacion

--- resources/views/admin/show_users.blade.php
+++ resources/views/admin/show_users.blade.php
@@ -1,61 +1,62 @@
 @extends('layouts.master')
 
 @section('title')
-	Dashboard
+    Dashboard
 @stop
 {{-- items for sidenavbar --}}
 
 @section('menu')
 
-	<li>
-		<a href="{{ route('users.index') }}"> <i class="fa fa-users"></i> Usuarios</a>
-		
-	</li>
+    <li>
+        <a href="{{ route('users.index') }}"> <i class="fa fa-users"></i> Usuarios</a>
+
+    </li>
 
 @stop
 
 @section('content')
 
-	<div class="row">
+    <div class="row">
         <div class="col-md-12">
-         	<div class="x_panel">
-            	<div class="x_content">
-					@foreach($users as $user)
-						<div class="col-md-4 col-sm-4 col-xs-12 animated fadeInDown">
-						    <div class="well profile_view">
-						        <div class="col-sm-12">
-						          	<h4 class="brief">
-						          		<i>{{ $user->profile->first_name }} {{ $user->profile->first_surname }}</i>
-						          	</h4>
-						          	<div class="left col-xs-12">
-						            	<ul class="list-unstyled">
-						              		<li>
-						              			<i class="fa fa-phone"></i> {{ $user->profile->number_phone }}
-						              		</li>
-						              		<li>
-						              			<i class="fa fa-envelope"></i> {{ $user->email}}
-						              		</li>
-						            		<li>
-						            			<i class="fa fa-child"></i> {{ $user->profile->hobby }}
-						            		</li>
-						            	</ul>
-						          	</div>
-						        </div>
-						        <div class="col-xs-12 bottom text-center">
-						          	<div class="col-xs-12 col-sm-6 emphasis">
-						            	<p class="ratings">
-											
-						            	</p>
-						          	</div>
-						          	<div class="col-xs-12 col-sm-6 emphasis">
-						            	<a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary btn-xs"><i class="fa fa-user"></i> Ver perfil</a>
-						          	</div>
-						        </div>
-						    </div>
-						</div>
-					@endforeach
+            <div class="x_panel">
+                <div class="x_content">
+                    @foreach($users as $user)
+                        <div class="col-md-4 col-sm-4 col-xs-12 animated fadeInDown">
+                            <div class="well profile_view">
+                                <div class="col-sm-12">
+                                    <h4 class="brief">
+                                        <i>{{ $user->profile->first_name }} {{ $user->profile->first_surname }}</i>
+                                    </h4>
+                                    <div class="left col-xs-12">
+                                        <ul class="list-unstyled">
+                                            <li>
+                                                <i class="fa fa-phone"></i> {{ $user->profile->number_phone }}
+                                            </li>
+                                            <li>
+                                                <i class="fa fa-envelope"></i> {{ $user->email}}
+                                            </li>
+                                            <li>
+                                                <i class="fa fa-child"></i> {{ $user->profile->hobby }}
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 bottom text-center">
+                                    <div class="col-xs-12 col-sm-6 emphasis">
+                                        <p class="ratings">
+
+                                        </p>
+                                    </div>
+                                    <div class="col-xs-12 col-sm-6 emphasis">
+                                        <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary btn-xs"><i
+                                                    class="fa fa-user"></i> Ver perfil</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
                 </div>
-			</div>
-		</div>
-	</div>
+            </div>
+        </div>
+    </div>
 @stop
--- resources/views/layouts/topnav.blade.php
+++ resources/views/layouts/topnav.blade.php
@@ -8,33 +8,37 @@
 
             <ul class="nav navbar-nav navbar-right">
                 <li class="">
-                    <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown"
+                       aria-expanded="false">
                         <img src="{{ asset('images/user.png')}}" alt=""> {{ ucfirst(Auth::user()->name) }}
                         <span class=" fa fa-angle-down"></span>
                     </a>
                     <ul class="dropdown-menu dropdown-usermenu animated fadeInDown pull-right">
                         <li>
-                        @if(Auth::user()->profile)
-                            <a href="{{ route('profile.show', Auth::user()->profile->id) }}"> Perfil</a>
-                        @else
-                            <a href="{{ route('profile.create') }}"> Perfil</a>
-                        @endif
+                            @if(Auth::user()->profile)
+                                <a href="{{ route('profile.show', Auth::user()->profile->id) }}"> Perfil</a>
+                            @else
+                                <a href="{{ route('profile.create') }}"> Perfil</a>
+                            @endif
                         </li>
                         <li>
                             <a href="javascript:;">Ajustes</a>
                         </li>
                         <li>
-                            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                <i class="fa fa-sign-out pull-right">  </i> Salir
+                            <a href="{{ route('logout') }}"
+                               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                <i class="fa fa-sign-out pull-right"> </i> Salir
                             </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                  style="display: none;">{{ csrf_field() }}
                             </form>
                         </li>
                     </ul>
                 </li>
 
                 <li role="presentation" class="dropdown">
-                    <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
+                    <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown"
+                       aria-expanded="false">
                         <i class="fa fa-shopping-cart cart"></i>
                         <span class="badge bg-green">
                             @php
@@ -47,32 +51,32 @@
                         </span>
                     </a>
                     <ul id="menu1" class="dropdown-menu list-unstyled msg_list animated fadeInDown" role="menu">
-                        
-                            @if(count($cart)>0)     
-                                @foreach($cart as $item)              
-                                    <li>
-                                        <a style="cursor: pointer;" href="{{ url('user/cart/show')}}">
+
+                        @if(count($cart)>0)
+                            @foreach($cart as $item)
+                                <li>
+                                    <a style="cursor: pointer;" href="{{ url('user/cart/show')}}">
                                             <span class="image">
-                                                <img src="{{ asset('images/user.png')}}" alt="Profile Image" />
+                                                <img src="{{ asset('images/user.png')}}" alt="Profile Image"/>
                                             </span>
-                                            <span>
+                                        <span>
                                                 <span>{{ $item->quantity." ".$item->name }}</span>
                                                 <span class="time">hace 3 min</span>
                                             </span>
-                                            <span class="message">
+                                        <span class="message">
                                                  agregado al carrito
                                             </span>
-                                        </a>
-                                    </li>
-                                @endforeach
-                            @else
-                                <li>
+                                    </a>
+                                </li>
+                            @endforeach
+                        @else
+                            <li>
                                     <span class="message">
                                         No tiene ningun producto agregado al carrito
                                     </span>
-                                </li>
-                            @endif
-                        
+                            </li>
+                        @endif
+
                         <li>
                             <div class="text-center">
                                 <a href="{{ url('user/cart/show') }}">
@@ -87,4 +91,4 @@
         </nav>
     </div>
 </div>
-      <!-- /top navigation -->
+<!-- /top navigation -->
