5db4a22fc906852cc6fb996d946afe42144ee5b6
AjxGnx <alirio1925@gmail.com>
2017-10-09 01:09:02 -0400

    [ADD] se agregaron campos fillable

--- app/Referred.php
+++ app/Referred.php
@@ -4,7 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Referred extends Model
 {
-    protected $fillable = ['user_id','name','last_name','telephone_number','relationship'];
+    protected $fillable = ['user_id','name','last_name','phone_number','relationship'];
 }
