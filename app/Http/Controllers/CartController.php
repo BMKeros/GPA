3afe86c8a9160593041fd0acf8c5478b5f75e0ea
Jesus Navas <jesusnavas2210@gmail.com>
2017-10-08 21:33:34 -0400

    [MOD] se modifico el controlador del carrito

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -24,8 +24,10 @@ class CartController extends Controller
 
     	$product = Cart::create([
     	    'user_id' => \Auth::user()->id,
-    	    'product_id' => $product->id,
-    	    'quantity' => $product->quantity = 1
+    	    'name' => $product->name,
+    	    'price' => $product->price,
+    	    'quantity' => $product->quantity = 1,
+    	    'image' => $product->image,
     	]);
 
     	return redirect('/user/cart/show');
