dea782c6f18217b355f69ad0f19c700588f2f023
AjxGnx <alirio1925@gmail.com>
2017-10-09 01:11:41 -0400

    [MOD] se soluciono un problema al guardar

--- resources/views/profiles/create.blade.php
+++ resources/views/profiles/create.blade.php
@@ -100,7 +100,6 @@
                             </div>
                         </div>
 
-
                         <div class="ln_solid"></div>
                         <div class="form-group">
                             <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
