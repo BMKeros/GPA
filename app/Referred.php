22c29a46316043d6ba3767e01a56a55775fef98c
AjxGnx <alirio1925@gmail.com>
2017-10-10 16:50:20 -0400

    [ADD] se agrego modelo relationship

--- app/Referred.php
+++ app/Referred.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Referred extends Model
 {
-    protected $fillable = ['user_id','name','last_name','phone_number','relationship'];
+    protected $fillable = ['user_id','name','last_name','phone_number','relationship_id'];
+
+    public function relationship()
+    {
+        return $this->belongsTo('App\Relationship');
+    }
 }
