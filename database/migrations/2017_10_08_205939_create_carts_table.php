2da82d2b2baba71d7eeca809a737b3039334fd72
Jesus Navas <jesusnavas2210@gmail.com>
2017-10-09 20:58:31 -0400

    [MOD] se modifico la migracion del carrito de compras

--- database/migrations/2017_10_08_205939_create_carts_table.php
+++ database/migrations/2017_10_08_205939_create_carts_table.php
@@ -16,10 +16,8 @@ class CreateCartsTable extends Migration
         Schema::create('carts', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
-            $table->string('name', 100);
-            $table->decimal('price');
+            $table->integer('product_id')->unsigned();
             $table->integer('quantity');
-            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
