289e436509f3b81375f1e034bdade6764b906850
boanergepro <antonycarrizo96@gmail.com>
2017-10-10 13:56:57 -0400

    [MOD] Eliminados lo botones Cancel y sutmit, creado el boton Crear.

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -109,8 +109,7 @@ new product
                     
                     <div class="form-group">
                       	<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-                        	<button type="submit" class="btn btn-primary">Cancel</button>
-                        	<button type="submit" class="btn btn-success">Submit</button>
+                        	<button type="submit" class="btn btn-success">Crear</button>
                       	</div>
                     </div>
 
