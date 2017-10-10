23589d7bdf4ed3be7bd4d94fb3562807077e0b44
noel <toyonoel@gmail.com>
2017-10-10 01:03:23 -0400

    [ADD] se agregaron vistas del catalogo

---
+++ resources/views/catalogue/show.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.master')
+
+
+@section('title')
+{{$product->name}}
+@stop
+
+@section('content')
+
+<div class="">
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="x_panel">
+              	<div class="x_title">
+                	<h2>Producto {{$product->name}} </h2>
+                	<div class="clearfix"></div>
+               	</div>
+                <div class="x_content">
+
+                    <div class="col-md-7 col-sm-7 col-xs-12">
+                    	<div class="product-image">
+                      		<img src="{{ asset('images/products/').'/'.$product->image }}" alt="..." />
+                    	</div>
+
+                        <h4>Descripcion del producto :</h4>
+                        <p style="width: 90%;">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua butcher retro keffiyeh dreamcatcher synth.{{$product->description}}</p>
+                  	</div>
+
+                  	<div class="col-md-5 col-sm-5 col-xs-12" style="border:0px solid #e5e5e5;">
+
+                    	<h3 class="prod_title">{{$product->name}} Marca  {{$product->brand}} </h3>
+
+                    	<div class="">
+                      		<h2>Categoria : {{$product->category->name}}</h2>
+                    	</div>                    
+
+                    	<div class="">
+                      		<h2>Cantidad: {{$product->quantity}} {{$product->unit->abbreviation}}</h2>
+                    	</div>
+
+                    	<div class="">
+                      		<h2>Cantidad disponible: {{$product->quantity_available}}</h2>
+                    	</div>
+                        <div class="">
+                            <h2>Limite por asociado:</h2>
+                            <span class="price-tax">{{$product->product_limit}}</span>
+                        </div>
+                    	<div class="">
+                      		<div class="product_price">
+                        		<h3 class="price" style="font-size:20px; color: #266BB9;"><b>Precio {{$product->price}} Bs<b></h3>
+                        		<span class="price-tax">asociado % {{$product->associated_percentage}}</span><br>
+                        		<span class="price-tax">calle % {{$product->street_percentage}}</span>
+                        		<br>
+                     		</div>
+                    	</div>
+                        <div class="">
+                            <button type="button" class="btn btn-default btn-lg">Add to Cart</button>
+                            <button type="button" class="btn btn-default btn-lg">Add to Wishlist</button>
+                        </div>
+                  	</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@stop
