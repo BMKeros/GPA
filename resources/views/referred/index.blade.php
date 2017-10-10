acd41ef0e2f61c183684508a48c3e3e4253c74f8
AjxGnx <alirio1925@gmail.com>
2017-10-10 18:05:34 -0400

    [MOD] se modifico la vista para darle mejor estetica

--- resources/views/referred/index.blade.php
+++ resources/views/referred/index.blade.php
@@ -6,37 +6,53 @@
 
 @section('content')
 
-    <div class="container">
-    <br />
-    @if (\Session::has('success'))
-      <div class="alert alert-success">
-        <p>{{ \Session::get('success') }}</p>
-      </div><br />
-     @endif
-    <table class="table table-striped">
-    <thead>
-      <tr>
-        <th>Nombre</th>
-        <th>Apellido</th>
-        <th>Telefono</th>
-        <th>Relacion</th>
-        
-      </tr>
-    </thead>
-    <tbody>
-      @foreach($referreds as $referred)
-       
-          <tr>
-            <td>{{ $referred->name }}</td>
-            <td>{{$referred->last_name}}</td>
-            <td>{{$referred->phone_number}}</td>
-            <td>{{$referred->relationship->name}}</td>
-        
-          </tr>
-        
-      @endforeach
-    </tbody>
-  </table>
+    <div class="clearfix"></div>
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="x_panel">
+                <div class="x_title">
+                    <h2>Referidos</h2>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                  <br />
+                  @if (\Session::has('success'))
+                    <div class="alert alert-success">
+                      <p>{{ \Session::get('success') }}</p>
+                    </div><br />
+                   @endif
+                  <table class="table table-striped">
+                  <thead>
+                    <tr>
+                      <th>Nombre</th>
+                      <th>Apellido</th>
+                      <th>Telefono</th>
+                      <th>Relacion</th>
+                      
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @forelse($referreds as $referred)
+                     
+                        <tr>
+                          <td>{{ $referred->name }}</td>
+                          <td>{{$referred->last_name}}</td>
+                          <td>{{$referred->phone_number}}</td>
+                          <td>{{$referred->relationship->name}}</td>
+                       
+                        </tr>
 
+                        @empty
+                          <tr>
+                            <td colspan="4"><h4 align="center">USTED NO POSEE REFERIDOS REGISTRADOS</h4></td>
+                          </tr>
+                                      
+                    @endforelse
+                  </tbody>
+                </table>
+              </div>
+            </div>
+        </div>
+    </div>
 
 @stop
