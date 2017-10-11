445f1d43864ffc30636a266a384778c3f6886682
boanergepro <antonycarrizo96@gmail.com>
2017-10-11 13:26:12 -0400

    [MOD] Cambiada por completo la vista del perfil.

---
+++ resources/views/admin/edit_users.blade.php
@@ -0,0 +1,129 @@
+@extends('layouts.master')
+
+@section('title')
+	Dashboard
+@stop
+{{-- items for sidenavbar --}}
+
+@section('menu')
+
+	<li>
+		<a href="{{ route('users.index') }}"> <i class="fa fa-users"></i> Usuarios</a>
+		
+	</li>
+
+@stop
+
+@section('content')
+
+    <div class="clearfix"></div>
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="x_panel">
+                <div class="x_title">
+                    <h2>Perfil</h2>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                    <br/>
+                    <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left" method="post"
+                          action="{{url('user/profile')}}">
+                        {{csrf_field()}}
+
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Primer Nombre
+                                <span class="required">*</span>
+                            </label>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                <input type="text" id="first-name" required="required"
+                                       class="form-control col-md-7 col-xs-12" name="first_name" value="{{ $user->profile->first_name  or ''}}">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="second-name">Segundo Nombre
+                                <span class="required">*</span>
+                            </label>
+
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                <input type="text" id="second-name" required="required"
+                                       class="form-control col-md-7 col-xs-12" name="second_name">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-surname">Primer Apellido
+                                <span class="required">*</span>
+                            </label>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                <input type="text" id="first-surname" required="required"
+                                       class="form-control col-md-7 col-xs-12" name="first_surname">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="second-surname">Segundo
+                                Apellido <span class="required">*</span>
+                            </label>
+
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                <input type="text" id="second-surname" required="required"
+                                       class="form-control col-md-7 col-xs-12" name="second_surname">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="cedula">Cedula <span
+                                        class="required">*</span>
+                            </label>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                <input type="text" id="cedula" required="required"
+                                       class="form-control col-md-7 col-xs-12" name="cedula">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="number-phone">Numero
+                                Telefonico <span class="required">*</span>
+                            </label>
+
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                <input type="text" id="number-phone" required="required"
+                                       class="form-control col-md-7 col-xs-12" name="number_phone">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="number-cellphone">Numero
+                                Celular <span class="required">*</span>
+                            </label>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                <input type="text" id="number-cellphone" required="required"
+                                       class="form-control col-md-7 col-xs-12" name="number_cellphone">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="hobby">Hobby <span
+                                        class="required">*</span>
+                            </label>
+
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                <input type="text" id="hobby" required="required"
+                                       class="form-control col-md-7 col-xs-12" name="hobby">
+                            </div>
+                        </div>
+
+
+                        <div class="ln_solid"></div>
+                        <div class="form-group">
+                            <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                                <button type="submit" class="btn btn-primary">Cancelar</button>
+                                <button type="submit" class="btn btn-success">Guardar</button>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@stop
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,69 +1,133 @@
-@extends('layouts.master')
+@extends('layouts.master') {{--Verificar esta linea--}}
 @section('title')
     Perfil
 @stop
 
-
 @section('content')
-
-    <body>
-    <div class="container">
-    <br />
-    @if (\Session::has('success'))
-      <div class="alert alert-success">
-        <p>{{ \Session::get('success') }}</p>
-      </div><br />
-     @endif
-    <table class="table table-striped">
-    
-      <tr>
-        <th>Primer Nombre</th>
-        <th>Segundo Nombre</th>
-        <th>Primer Apellido</th>
-        <th>Segundo Apellido</th>        
-      </tr>
-    
-    <tbody>
-      <tr>
-        <td>{{$profile->first_name}}</td>
-        <td>{{$profile->second_name}}</td>
-        <td>{{$profile->first_surname}}</td>
-        <td>{{$profile->second_surname}}</td>
-      </tr>
-     
-    </tbody>
-  </table>
-
-    <table class="table table-striped">
-    
-      <tr>
-        <th>Cedula</th>
-        <th>Numero Telefonico</th>
-        <th>Numero Celular</th>
-        <th>Hobby</th>        
-      </tr>
-    
-    <tbody>
-      <tr>
-        <td>{{$profile->cedula}}</td>
-        <td>    {{$profile->number_phone}}</td>
-        <td>{{$profile->number_cellphone}}</td>
-        <td>{{$profile->hobby}}</td>
-      </tr>
-     
-    </tbody>
-  </table>
-
-   <div class="ln_solid"></div>
-                        <div class="form-group">
-                            <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-11">
-                               <a href="{{action('ProfileController@edit', $profile['id'])}}" class="btn btn-primary">Editar</a> 
+    <div class="">
+        @if (\Session::has('success'))
+            <div class="alert alert-success">
+                <p>{{ \Session::get('success') }}</p>
+            </div>
+            <br />
+        @endif
+        <div class="page-title">
+            <div class="title_left">
+                <h3>Perfir del usuario</h3>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                    <div class="x_title">
+                
+                        <ul class="nav navbar-right panel_toolbox">
+                            <li>
+                                <a href="#"><i class="fa fa-chevron-up"></i></a>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="#">Settings 1</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Settings 2</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-close"></i>
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="x_content">
+                        <div class="col-md-3 col-sm-3 col-xs-12 profile_left">
+                            <div class="profile_img">
+                                <div id="crop-avatar">
+                                    <div class="avatar-view" title="Change the avatar">
+                                        <img src="{{ asset('images/user.png') }}" alt="Avatar">
+                                    </div>
+                                    <h3>{{ $profile->first_name }} {{ $profile->first_surname }}</h3>
+                                    <a href="{{ route('profile.edit', $profile->id) }}" class="btn btn-success"><i class="fa fa-edit m-right-xs"></i>Editar perfil</a>
+                                </div>
                             </div>
                         </div>
-  
-        
-  </div>
-  </body>
-
-
+                        <div class="col-md-9 col-sm-9 col-xs-12">
+                            <div class="profile_title">
+                                <div class="col-md-6">
+                                    <h2>Datos del perifil</h2>
+                                </div>
+                            </div>
+                            <div class="x_content">
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                    <div class="col-md-8 col-sm-8 col-xs-12">
+                                        <h2><b>Primer nombre:</b></h2>
+                                        <h2><b>Segundo nombre:</b></h2>
+                                        <h2><b>Primer apellido:</b></h2>
+                                        <h2><b>Segundo apellido:</b></h2>
+                                    </div>
+                                    <div class="col-md-4 col-sm-4 col-xs-12" style="margin-left: -25px;">
+                                        <h2>{{ $profile->first_name }}</h2>
+                                        <h2>{{ $profile->second_name }}</h2>
+                                        <h2>{{ $profile->first_surname }}</h2>
+                                        <h2>{{ $profile->second_surname }}</h2>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                    <div class="col-md-8 col-sm-8 col-xs-12">
+                                        <h2><b>CI:</b></h2>
+                                        <h2><b>Numero celular:</b></h2>
+                                        <h2><b>Numero telefono:</b></h2>
+                                        <h2><b>Hobby:</b></h2>
+                                    </div>
+                                    <div class="col-md-4 col-sm-4 col-xs-12" style="margin-left: -25px;">
+                                        <h2>{{ $profile->cedula }}</h2>
+                                        <h2>{{ $profile->number_phone }}</h2>
+                                        <h2>{{ $profile->number_cellphone }}</h2>
+                                        <h2>{{ $profile->hobby }}</h2>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="" role="tabpanel" data-example-id="togglable-tabs">
+                                <div class="col-md-12 col-sm-12 col-xs-12" style="margin-top: 30px;">
+                                    <div class="profile_title">
+                                        <div class="col-md-6">
+                                            <h2>Actividades</h2>
+                                        </div>
+                                     </div>
+                                
+                        
+                                
+                                <div id="myTabContent" class="tab-content">
+                                    <div role="tabpanel" class="tab-pane fade active in" id="tab_content1" aria-labelledby="home-tab">
+                                        <ul class="messages">
+                                             <li>
+                                                <img src="{{ asset('images/user.png') }}" class="avatar" alt="Avatar">
+                                                <div class="message_date">
+                    
+                                                </div>
+                                                <div class="message_wrapper">
+                                                    <h4 class="heading">Solicitud</h4>
+                                                    <blockquote class="message">3 kilogramos de maiz blanco</blockquote>
+                                                    <br />
+                                                    <p class="url">
+                                                        <span class="fs1 text-info" aria-hidden="true" data-icon=""></span>
+                                                    </p>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
