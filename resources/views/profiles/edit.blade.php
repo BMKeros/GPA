acfe907a4190b595319096d36cfc626438778a63
AjxGnx <alirio1925@gmail.com>
2017-10-13 13:25:32 -0400

    [MOD] se acomodo la identacion del template

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -8,122 +8,133 @@
 
 
 @section('content')
-
+    
  <div class="container">
-      <h2>Editar Perfil</h2><br  />
-      @if ($errors->any())
-      <div class="alert alert-danger">
-          <ul>
-              @foreach ($errors->all() as $error)
-                  <li>{{ $error }}</li>
-              @endforeach
-          </ul>
-      </div><br />
-      @endif
-      <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left" method="post"
-                          action="{{action('ProfileController@update', $profile['id'])}}">
-                          <input name="_method" type="hidden" value="PATCH">
-                        {{csrf_field()}}
-
-                        <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Primer Nombre
-                                <span class="required">*</span>
-                            </label>
-                            <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" id="first-name" required="required"
-                                       class="form-control col-md-7 col-xs-12" name="first_name" value="{{$profile->first_name}}">
-                            </div>
+    <div class="clearfix"></div>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div><br />
+        @endif
+        <div class="clearfix"></div>
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="x_panel">
+                        <div class="x_title">
+                            <h2>Editar Perfil</h2>
+                                <div class="clearfix"></div>
                         </div>
-                        <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="second-name">Segundo Nombre
-                                <span class="required">*</span>
-                            </label>
+                        <div class="x_content">
+                            <br/>
+                            <form id="demo-form2" data-parsley-validate class=" form-horizontal form-label-left" method="post"
+                            action="{{action('ProfileController@update', $profile['id'])}}">
+                                <input name="_method" type="hidden" value="PATCH">
+                                {{csrf_field()}}
 
-                            <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" id="second-name" required="required"
-                                       class="form-control col-md-7 col-xs-12" name="second_name" value="{{$profile->second_name}}">
-                            </div>
-                        </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Primer Nombre
+                                    <span class="required">*</span>
+                                    </label>
+                                    <div class="col-md-6 col-sm-6 col-xs-12">
+                                        <input type="text" id="first-name" required="required"
+                                        class="form-control col-md-7 col-xs-12" name="first_name" value="{{$profile->first_name}}">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="second-name">Segundo Nombre
+                                    <span class="required">*</span>
+                                    </label>
 
-                        <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-surname">Primer Apellido
-                                <span class="required">*</span>
-                            </label>
-                            <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" id="first-surname" required="required"
-                                       class="form-control col-md-7 col-xs-12" name="first_surname" value="{{$profile->first_surname}}">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="second-surname">Segundo
-                                Apellido <span class="required">*</span>
-                            </label>
+                                    <div class="col-md-6 col-sm-6 col-xs-12">
+                                        <input type="text" id="second-name" required="required"
+                                        class="form-control col-md-7 col-xs-12" name="second_name" value="{{$profile->second_name}}">
+                                    </div>
+                                </div>
 
-                            <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" id="second-surname" required="required"
-                                       class="form-control col-md-7 col-xs-12" name="second_surname" value="{{$profile->second_surname}}">
-                            </div>
-                        </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-surname">Primer Apellido
+                                    <span class="required">*</span>
+                                    </label>
+                                    <div class="col-md-6 col-sm-6 col-xs-12">
+                                        <input type="text" id="first-surname" required="required"
+                                        class="form-control col-md-7 col-xs-12" name="first_surname" value="{{$profile->first_surname}}">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="second-surname">Segundo
+                                    Apellido <span class="required">*</span>
+                                    </label>
+                                    <div class="col-md-6 col-sm-6 col-xs-12">
+                                        <input type="text" id="second-surname" required="required"
+                                        class="form-control col-md-7 col-xs-12" name="second_surname" value="{{$profile->second_surname}}">
+                                    </div>
+                                </div>
 
-                        <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="cedula">Cedula <span
+                                <div class="form-group">
+                                    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="cedula">Cedula <span
                                         class="required">*</span>
-                            </label>
-                            <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" id="cedula" required="required"
-                                       class="form-control col-md-7 col-xs-12" name="cedula" value="{{$profile->cedula}}">
-                            </div>
+                                    </label>
+                                    <div class="col-md-6 col-sm-6 col-xs-12">
+                                        <input type="text" id="cedula" required="required"
+                                        class="form-control col-md-7 col-xs-12" name="cedula" value="{{$profile->cedula}}">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="number-phone">Numero
+                                    Telefonico <span class="required">*</span>
+                                    </label>
+
+                                    <div class="col-md-6 col-sm-6 col-xs-12">
+                                        <input type="text" id="number-phone" required="required"
+                                        class="form-control col-md-7 col-xs-12" name="number_phone" value="{{$profile->number_phone}}">
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="number-cellphone">Numero
+                                    Celular <span class="required">*</span>
+                                    </label>
+                                    <div class="col-md-6 col-sm-6 col-xs-12">
+                                        <input type="text" id="number-cellphone" required="required"
+                                        class="form-control col-md-7 col-xs-12" name="number_cellphone" value="{{$profile->number_cellphone}}">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="hobby">Hobby <span
+                                    class="required">*</span>
+                                    </label>
+
+                                    <div class="col-md-6 col-sm-6 col-xs-12">
+                                        <input type="text" id="hobby" required="required"
+                                        class="form-control col-md-7 col-xs-12" name="hobby" value="{{$profile->hobby}}">
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                                        <a href="{{action('ProfileController@show', $profile['id'])}}" class="btn btn-primary">Atras</a>	
+                                        <button type="submit" class="btn btn-success">Actualizar</button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                        <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="number-phone">Numero
-                                Telefonico <span class="required">*</span>
-                            </label>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-                            <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" id="number-phone" required="required"
-                                       class="form-control col-md-7 col-xs-12" name="number_phone" value="{{$profile->number_phone}}">
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="number-cellphone">Numero
-                                Celular <span class="required">*</span>
-                            </label>
-                            <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" id="number-cellphone" required="required"
-                                       class="form-control col-md-7 col-xs-12" name="number_cellphone" value="{{$profile->number_cellphone}}">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="hobby">Hobby <span
-                                        class="required">*</span>
-                            </label>
-
-                            <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" id="hobby" required="required"
-                                       class="form-control col-md-7 col-xs-12" name="hobby" value="{{$profile->hobby}}">
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-                                <a href="{{action('ProfileController@show', $profile['id'])}}" class="btn btn-primary">Atras</a>	
-                                <button type="submit" class="btn btn-success">Actualizar</button>
-                            </div>
-                        </div>
-
-                        </form>
-
-    @if (count($errors) > 0)
-        <div class="alert alert-danger">
-            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-            <ul>
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                <ul>
                 @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
                 @endforeach
-            </ul>
-        </div>
-    @endif
-
-
-@stop
+                </ul>
+            </div>
+        @endif
+    @stop
