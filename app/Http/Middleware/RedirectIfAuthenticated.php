7aebe2431a8f9de6c176e052e849b827dee45749
AjxGnx <alirio1925@gmail.com>
2017-11-21 10:12:04 -0400

    [MOD] se modifico logica para redirec

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            if (Auth::user()->role->id == 1) {
+            if (\Auth::user()->hasRole('ADMIN')){
                 return redirect('/admin/dashboard');
             }
             return redirect('/client/dashboard');
