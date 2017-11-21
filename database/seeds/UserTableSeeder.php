3571a1097b26e0163475f66b67aeb0b96da54212
AjxGnx <alirio1925@gmail.com>
2017-11-21 10:11:22 -0400

    [MOD] se modifico seed user

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -14,23 +14,23 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         $data = [
-            [
+            [   
+                'id' => 1,
                 'name' => 'admin',
                 'email' => 'admin@bmkeros.org.ve',
                 'password' => bcrypt('admin'),
-                'role_id' => 1
             ],
-            [
+            [   
+                'id' => 2,
                 'name' => 'user',
                 'email' => 'user@bmkeros.org.ve',
                 'password' => bcrypt('user'),
-                'role_id' => 2
             ],
             [
+                'id' => 3,
                 'name' => 'socio',
                 'email' => 'socio@bmkeros.org.ve',
                 'password' => bcrypt('socio'),
-                'role_id' => 3
             ],
         ];
 
