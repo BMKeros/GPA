2089455480545a140a68757bc879e0ef2b32d640
Boanerget <antonyprogramacion96@gmail.com>
2017-11-23 13:59:50 -0400

    [MOD] Agregada la relacion con el modelo User.

--- app/Profile.php
+++ app/Profile.php
@@ -10,4 +10,8 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
    protected $fillable = ['user_id','first_name','second_name','first_surname','second_surname','cedula','number_phone','number_cellphone','hobby'];
+   public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
