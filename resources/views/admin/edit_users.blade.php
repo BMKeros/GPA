0ed2ea467d66d99b768bd41a66d68dbfde054368
Boanerget <antonyprogramacion96@gmail.com>
2017-11-24 14:39:46 -0400

    [FIX] Cambiado el action del formulario y agregados los elementos del sidenavbar.

--- resources/views/admin/edit_users.blade.php
+++ resources/views/admin/edit_users.blade.php
@@ -1,17 +1,7 @@
-@extends('layouts.master')
+@extends('layouts.master_admin')
 
 @section('title')
-	Dashboard
-@stop
-{{-- items for sidenavbar --}}
-
-@section('menu')
-
-	<li>
-		<a href="{{ route('users.index') }}"> <i class="fa fa-users"></i> Usuarios</a>
-		
-	</li>
-
+	Editar usuario
 @stop
 
 @section('content')
@@ -21,100 +11,65 @@
         <div class="col-md-12 col-sm-12 col-xs-12">
             <div class="x_panel">
                 <div class="x_title">
-                    <h2>Perfil</h2>
+                    <h2>Editar datos del usuario</h2>
                     <div class="clearfix"></div>
                 </div>
                 <div class="x_content">
                     <br/>
-                    <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left" method="post"
-                          action="{{url('user/profile')}}">
-                        {{csrf_field()}}
-
+                    <form id="demo-form2" data-parsley-validate class=" form-horizontal form-label-left" method="POST"
+                            action="{{action('AdministrationUserController@update', $user['id'])}}">
+                        <input name="_method" type="hidden" value="PATCH">
+                        {{ csrf_field() }}
                         <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Primer Nombre
-                                <span class="required">*</span>
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Nombre de usuario
+                            <span class="required">*</span>
                             </label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" id="first-name" required="required"
-                                       class="form-control col-md-7 col-xs-12" name="first_name" value="{{ $user->profile->first_name  or ''}}">
+                                <input type="text" id="name" required="required"
+                                class="form-control col-md-7 col-xs-12" name="name" value="{{ $user->name }}">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="second-name">Segundo Nombre
-                                <span class="required">*</span>
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="email">Email 
+                            <span class="required">*</span>
                             </label>
 
                             <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" id="second-name" required="required"
-                                       class="form-control col-md-7 col-xs-12" name="second_name">
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-surname">Primer Apellido
-                                <span class="required">*</span>
-                            </label>
-                            <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" id="first-surname" required="required"
-                                       class="form-control col-md-7 col-xs-12" name="first_surname">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="second-surname">Segundo
-                                Apellido <span class="required">*</span>
-                            </label>
-
-                            <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" id="second-surname" required="required"
-                                       class="form-control col-md-7 col-xs-12" name="second_surname">
+                                <input type="email" id="email" required="required"
+                                class="form-control col-md-7 col-xs-12" name="email" value="{{ $user->email }}">
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="cedula">Cedula <span
-                                        class="required">*</span>
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="password">Contraseña
                             </label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" id="cedula" required="required"
-                                       class="form-control col-md-7 col-xs-12" name="cedula">
+                                <input type="password" id="password" class="form-control col-md-7 col-xs-12" name="password">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="number-phone">Numero
-                                Telefonico <span class="required">*</span>
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="password_confirmation">Repita la contraseña
                             </label>
-
                             <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" id="number-phone" required="required"
-                                       class="form-control col-md-7 col-xs-12" name="number_phone">
+                                <input type="password" id="password_confirmation"
+                                class="form-control col-md-7 col-xs-12" name="password_confirmation">
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="number-cellphone">Numero
-                                Celular <span class="required">*</span>
-                            </label>
-                            <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" id="number-cellphone" required="required"
-                                       class="form-control col-md-7 col-xs-12" name="number_cellphone">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="hobby">Hobby <span
-                                        class="required">*</span>
-                            </label>
-
-                            <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" id="hobby" required="required"
-                                       class="form-control col-md-7 col-xs-12" name="hobby">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12">Rol</label>
+                            <div class="col-md-6 col-sm-9 col-xs-12">
+                                <select class="select2_single form-control" tabindex="-1" name="rol">
+                                    <option value="0" selected> -- Seleccione --</option>
+                                    @foreach($roles as $rol)
+                                        <option value="{{ $rol->id }}">{{ $rol->name }}</option>
+                                    @endforeach
+                                </select>
                             </div>
                         </div>
 
-
-                        <div class="ln_solid"></div>
                         <div class="form-group">
                             <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-                                <button type="submit" class="btn btn-primary">Cancelar</button>
                                 <button type="submit" class="btn btn-success">Guardar</button>
                             </div>
                         </div>
