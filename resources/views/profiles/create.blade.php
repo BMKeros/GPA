5b1c3cb879ef4445b768827b7f88bd82542c8222
Boanerget <antonyprogramacion96@gmail.com>
2017-11-24 14:57:42 -0400

    [MOD] Cambios menores.

--- resources/views/profiles/create.blade.php
+++ resources/views/profiles/create.blade.php
@@ -100,6 +100,7 @@
                             </div>
                         </div>
 
+                        <input type="hidden" name="user_id" value="{{ $user_id }}">
                         <div class="ln_solid"></div>
                         <div class="form-group">
                             <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
