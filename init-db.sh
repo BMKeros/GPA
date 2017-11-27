83fcbd5ecadcac5cccc24d3bb8a2eb1d9c474d33
Boanerget <antonyprogramacion96@gmail.com>
2017-11-27 17:09:35 -0400

    [ADD] sh para preparar la base de datos.

---
+++ init-db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+
+echo "Ejecutando migraciones"
+php artisan migrate
+
+echo "Ejecutando dumpautoload = CREANDO COMPILADOS"
+composer dump-autoload
+
+echo "Ejecutando autollenado de la base de datos"
+php artisan db:seed
+
+echo "Base de datos inicializada"
+echo "Adios"
