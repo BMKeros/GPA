efcd4ea3e89cbed92c6ce0bc414a193192795f29
Israel Lugo <hostelixisrael@gmail.com>
2017-11-29 10:16:03 -0400

    [MOD] se quito el campo id estatico del seed

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -14,20 +14,17 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         User::insert([
-            [   
-                'id' => 1,
+            [
                 'name' => 'admin',
                 'email' => 'admin@bmkeros.org.ve',
                 'password' => bcrypt('admin'),
             ],
-            [   
-                'id' => 2,
+            [
                 'name' => 'user',
                 'email' => 'user@bmkeros.org.ve',
                 'password' => bcrypt('user'),
             ],
             [
-                'id' => 3,
                 'name' => 'socio',
                 'email' => 'socio@bmkeros.org.ve',
                 'password' => bcrypt('socio'),
