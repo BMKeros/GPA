dd6c487b311819b0331e3c24f6937031b652c4cf
Boanerget <antonyprogramacion96@gmail.com>
2017-11-29 13:28:22 -0400

    [ADD][MOD] Vista para mostrar el error 404.

---
+++ resources/views/layouts/page_404.blade.php
@@ -0,0 +1,17 @@
+@section('title')
+    Error 404
+@stop
+
+@include('layouts.head')
+
+@section('content')
+	<div class="col-md-12">
+	    <div class="col-middle">
+	        <div class="text-center text-center">
+	            <h1 class="error-number">404</h1>
+	            <h2>Error recurso no encontrado</h2>
+	            <p>Usted esta intentando acceder a un recurso inexistente, por favor verifique su solicitud.</a>
+	            </p>
+	        </div>
+	    </div>
+	</div>
