7931e8c16bc8f1c098a4814a7018275ccdc7caf1
noel <toyonoel@gmail.com>
2017-11-30 09:49:10 -0400

    [ADD] si el producto existe en el inv se actualiza la cantidad disponible

--- app/Http/Controllers/AdministrationInventoryController.php
+++ app/Http/Controllers/AdministrationInventoryController.php
@@ -38,15 +38,26 @@ class AdministrationInventoryController extends Controller
     public function store(Request $request)
     {
         $data = $request->all();
-        $product = Product::findOrFail($data['product']);
-        $slug = $product->slug;
-        $inventory = Inventory::create([
-            'status_id' => $data['status'],
-            'product_id' => $data['product'],
-            'unit_id' => $data['unit'],
-            'slug' => $slug,
-            'quantity_available' => $data['quantity'],
-        ]);
+
+        $product_exist = Inventory::where('product_id', $data['product'])->firstOrFail();
+
+        if (!is_null($product_exist)){
+
+            $product_exist->quantity_available = $product_exist->quantity_available + $data['quantity'];
+            $product_exist->save();
+
+        }
+        else{
+            $product = Product::findOrFail($data['product']);
+            $slug = $product->slug;
+            $inventory = Inventory::create([
+                'status_id' => $data['status'],
+                'product_id' => $data['product'],
+                'unit_id' => $data['unit'],
+                'slug' => $slug,
+                'quantity_available' => $data['quantity'],
+            ]);
+        }
 
     
         return redirect()->route('inventory.index');
@@ -70,6 +81,7 @@ class AdministrationInventoryController extends Controller
 
         $inventory->product_id = $request->product;
         $inventory->quantity_available = $request->quantity;
+        $inventory->status_id = $request->status;
         $inventory->unit_id = $request->unit;
         $inventory->slug = $slug;
         $inventory->save();
