2010e9012de9781c93a09f87f0f1c3f5079c71b6
noel <toyonoel@gmail.com>
2017-11-29 23:18:51 -0400

    [ADD] se agregaron los errores de las validaciones

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -9,7 +9,18 @@
 
 
 @section('content')
-
+<div class="clearfix"></div>
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Error!</strong>
+        <ul>
+    @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+    @endforeach
+        </ul>
+    </div>
+@endif
+<div class="clearfix"></div>
 <div class="row">
     <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="x_panel">
@@ -44,8 +55,13 @@
                                     <option value="{{ $category->id }}">{{ $category->name }}</option>
                                     @endif
                                 @else
+                                    @if (old('category') == $category->id )
+                                    <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
+
+                                    @else
                                     <option value="{{ $category->id }}">{{ $category->name }}</option>
-                                    
+                                    @endif
+       
                                 @endif                               
                             @endforeach
 
@@ -56,7 +72,7 @@
                     	<label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Nombre <span class="required">*</span>
                    		</label>
                 		<div class="col-md-6 col-sm-6 col-xs-12">
-                        <input type="text" id="name" required="required" class="form-control col-md-7 col-xs-12" name="name" value="@if ($product == null) @else {{$product->name}} @endif">
+                        <input type="text" id="name" required="required" class="form-control col-md-7 col-xs-12" name="name" value="@if ($product == null) {{ old('name') }} @else {{$product->name}} @endif">
                		 	</div>
                		</div>
                		<div class="form-group">
@@ -64,25 +80,25 @@
                       	</label>
                			
                			<div class="col-md-6 col-sm-6 col-xs-12">
-                    		<input type="text" id="brand" name="brand" required="required" class="form-control col-md-7 col-xs-12" value="@if ($product == null) @else {{$product->brand}} @endif">
+                    		<input type="text" id="brand" name="brand" required="required" class="form-control col-md-7 col-xs-12" value="@if ($product == null) {{ old('brand') }} @else {{$product->brand}} @endif">
                 		</div>
                 	</div>
 
                     <div class="form-group">
                         <label for="price" class="control-label col-md-3 col-sm-3 col-xs-12">Precio<span class="required">*</span></label>
                         <div class="col-md-6 col-sm-6 col-xs-12">
-                            <input id="price" class="form-control col-md-7 col-xs-12" type="text" name="price" value="@if (is_null($product)) @else {{$product->price}} @endif">
+                            <input id="price" class="form-control col-md-7 col-xs-12" type="text" name="price" value="@if (is_null($product)) {{ old('price') }} @else {{$product->price}} @endif">
                         </div>
                     </div>
                     <div class="form-group">
 
                             <label class="control-label col-md-3 col-sm-3 col-xs-12">Porcentaje<span class="required">*</span></label>
                         <div class="col-md-3 col-sm-6 col-xs-12">
-                            <input id="precio-name" class="form-control col-md-7 col-xs-12" type="text" name="associated_percentage"  placeholder="Asociado" value="@if (is_null($product)) @else {{$product->associated_percentage}} @endif">
+                            <input id="precio-name" class="form-control col-md-7 col-xs-12" type="text" name="associated_percentage"  placeholder="Asociado" @if (is_null($product)) value="{{ old('associated_percentage') }}" @else value="{{$product->associated_percentage}}" @endif>
                         </div>
 
                         <div class="col-md-3 col-sm-6 col-xs-12">
-                            <input id="precio-name" class="form-control col-md-7 col-xs-12" type="text" name="street_percentage" placeholder="Calle" value="@if (is_null($product)) @else {{$product->street_percentage}} @endif">
+                            <input id="precio-name" class="form-control col-md-7 col-xs-12" type="text" name="street_percentage" placeholder="Calle" @if (is_null($product)) value="{{ old('street_percentage') }}" @else value="{{$product->street_percentage}}" @endif>
                         </div>
 
                     </div>
@@ -91,22 +107,21 @@
                     	<label class="control-label col-md-3 col-sm-3 col-xs-12" for="image">imagen <span class="required">*</span>
                       	</label>
                       	<div class="col-md-6 col-sm-6 col-xs-12">
-                        	<input id="image" class="form-control" type="file" name="image">
+                        	<input id="image" class="form-control" type="file" name="image" value="{{ old('image') }}">
                       	</div>
                     </div>
                     <div class="form-group">
                         <label class="control-label col-md-3 col-sm-3 col-xs-12">Descripcion <span class="required" for="description">*</span>
                         </label>
                         <div class="col-md-6 col-sm-6 col-xs-12">
-                            <textarea id="description" required="required" class="form-control" name="description">@if (is_null($product)) @else {{$product->description}} @endif</textarea>
+                            <textarea id="description" required="required" class="form-control" name="description">@if (is_null($product)){{ old('description') }} @else {{$product->description}} @endif</textarea>
                         </div>
                     </div>
 
-                    <br><br>
                     <div class="ln_solid"></div>
                     
                     <div class="form-group">
-                      	<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                      	<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-5">
                         	<button type="submit" class="btn btn-success">@if (is_null($product)) Crear @else Guardar @endif</button>
                       	</div>
                     </div>
@@ -114,6 +129,7 @@
                 </form>
             </div>
         </div>
+
     </div>
 </div>
 
