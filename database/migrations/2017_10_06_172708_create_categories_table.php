2c033027fd0a2896264a9d40ac7047802ea79c11
noel <toyonoel@gmail.com>
2017-11-29 23:16:27 -0400

    [MOD] se modifico el campo description a nulleable

--- database/migrations/2017_10_06_172708_create_categories_table.php
+++ database/migrations/2017_10_06_172708_create_categories_table.php
@@ -16,7 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('category', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('description');
+            $table->string('description')->nullable();
             $table->string('slug');
 
             $table->timestamps();
