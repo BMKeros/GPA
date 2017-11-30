266d7e59076969ad5d53b0f6a828ac4f567b1ea9
noel <toyonoel@gmail.com>
2017-11-29 23:17:08 -0400

    [ADD] se le agrego los errores

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -10,7 +10,17 @@
 
 
 @section('content')
-
+<div class="clearfix"></div>
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Error!</strong>
+        <ul>
+    @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+    @endforeach
+        </ul>
+    </div>
+@endif
 <div class="clearfix"></div>
 <div class="row">
     <div class="col-md-12 col-sm-12 col-xs-12">
@@ -39,7 +49,7 @@
                     	<label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Nombre <span class="required">*</span>
                    		</label>
                 		<div class="col-md-6 col-sm-6 col-xs-12">
-                        <input type="text" id="first-name" required="required" class="form-control col-md-7 col-xs-12" name="name" value="@if ($category == null) @else {{$category->name}} @endif">
+                        <input type="text" id="first-name" required="required" class="form-control col-md-7 col-xs-12" name="name" value="@if ($category == null) {{ old('name') }} @else {{$category->name}} @endif">
                		 	</div>
                		</div>
                		<div class="form-group">
@@ -47,11 +57,11 @@
                       	</label>
                			
                			<div class="col-md-6 col-sm-6 col-xs-12">
-                    		<input type="text" id="last-name" name="description" required="required" class="form-control col-md-7 col-xs-12" value="@if ($category == null) @else {{$category->description}} @endif">
+                    		<input type="text" id="last-name" name="description" required="required" class="form-control col-md-7 col-xs-12" value="@if ($category == null) {{ old('description') }} @else {{$category->description}} @endif">
                 		</div>
                 	</div>
                     <div class="form-group">
-                      	<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-4">
+                      	<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-5">
                         	<button type="submit" class="btn btn-primary">@if ($category == null) Crear @else Guardar @endif</button>
                       	</div>
 
