2239f71c685c662570ef3e0afdaad7e6f0397124
Jesus Navas <jesusnavas2210@gmail.com>
2017-12-05 01:46:33 -0400

    [MOD] se mejoro la vista del carrito

--- resources/views/cart/show.blade.php
+++ resources/views/cart/show.blade.php
@@ -6,137 +6,106 @@
 
 @section('content')
 
-@php
-    $cart = \Session::get('cart');
-    $user = "";
-    $subtotal = 0;
-    $porciento = 0;
-    $porcentaje = 0;
-    $total = 0;
-@endphp
+    @php
+        $cart = \Session::get('cart');
+        $subtotal = 0;
+        $porciento = 0;
+        $porcentaje = 0;
+    @endphp
 
     <div class="clearfix"></div>
+    <div class="page-title">
+        <div class="title_left">
+            <h3>Carrito de Compras</h3>
+        </div>
+
+        
+    </div>
     <div class="row">
         <div class="col-md-12">
-            <div class="x_panel">
-                <div class="x_title">
-                    <h2>Datos del carrito</h2>
-                    <div class="clearfix"></div>
-                </div>
 
+            <div class="x_panel">
+                @if(count($cart)>0)
+                    <p class="x_title text-center">
+                        <a href="{{ route('cart.trash') }}" class="btn btn-danger text-center">
+                            Vaciar carrito <i class="fa fa-trash"></i>
+                        </a>
+                    </p>
+                @endif
                 <div class="x_content">
                     <!-- title row -->
-
-                    <div class="row">
-                        <div class="col-xs-12 invoice-header">
-                            <h1>Invoice. <small class="pull-right">Date:
-                            16/08/2016</small></h1>
-                        </div><!-- /.col -->
-                    </div><!-- info row -->
-
-                    <div class="row invoice-info">
-                        <div class="col-sm-4 invoice-col">
-                            De
-                            <address>
-                                <strong>A.C Bmkeros R.L</strong><br />
-                                795 Freedom Ave, Suite 600<br />
-                                New York, CA 94107<br />
-                                Phone: 1 (804) 123-9876<br />
-                                Email: ironadmin.com
-                            </address>
-                        </div><!-- /.col -->
-
-                        <div class="col-sm-4 invoice-col">
-                            Para
-
-                            @foreach($cart as $item)
-                                @php
-                                    $user = $item->user->name;
-                                @endphp
-                            @endforeach
-
-                            <address>
-                                <strong>{{ $user }}</strong><br />
-                                795 Freedom Ave, Suite 600<br />
-                                New York, CA 94107<br />
-                                Phone: 1 (804) 123-9876<br />
-                                Email: jon@ironadmin.com
-                            </address>
-                        </div><!-- /.col -->
-
-                        <div class="col-sm-4 invoice-col">
-                            <b>Pedido #007612</b><br />
-                            <br />
-                            <b>Order ID:</b> 4F3S8J<br />
-                            <b>Payment Due:</b> 2/22/2014<br />
-                            <b>Account:</b> 968-34567
-                        </div><!-- /.col -->
-                    </div><!-- /.row -->
-                    <!-- Table row -->
-
                     <div class="row">
                         <div class="col-xs-12 table">
-                            <table class="table table-striped">
-                                <thead>
-                                    <tr>
-                                        <th>Imagen</th>
+                            @if(count($cart)>0)
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>Imagen</th>
 
-                                        <th>Producto</th>
+                                            <th>Producto</th>
 
-                                        <th>Precio</th>
+                                            <th>Precio</th>
 
-                                        <th>Cantidad</th>
+                                            <th>Cantidad</th>
 
-                                        <th>Subtotal</th>
-                                    </tr>
-                                </thead>
+                                            <th>Subtotal</th>
 
-                                <tbody>
+                                            <th>Quitar</th>
+                                        </tr>
+                                    </thead>
 
-                                    @if(count($cart)>0)
-                                        @foreach($cart as $item)
-                                            <tr>
-                                                <td><img src="{{ $item->product->image }}" alt=""></td>
+                                    <tbody>
 
-                                                <td>{{ $item->product->name }}</td>
+                                        
+                                            @foreach($cart as $item)
+                                                <tr>
+                                                    <td><img style="width: 40px; height: 40px;" src="{{ asset('images/products/').'/'.$item->product->image }}" alt=""></td>
+                                                    
+                                                    <td>{{ $item->product->name }}</td>
 
-                                                <td>{{ number_format($item->product->price,2) }}</td>
+                                                    <td>{{ number_format($item->product->price,2) }}</td>
 
-                                                <td>{{ $item->quantity }}</td>
+                                                    <td>
+                                                        <input 
+                                                            type="number"
+                                                            min="1"
+                                                            max="100"
+                                                            value="{{ $item->quantity }}"
+                                                            id="product_{{ $item->product->id }}"
+                                                            style="width: 80px;" 
+                                                        >
 
-                                                <td>{{ number_format($item->product->price * $item->quantity,2) }}</td>
-                                            </tr>
-                                        @endforeach
-                                    @endif
-                                </tbody>
-                            </table>
-                                @if(count($cart)<1)
-                                    <p style="text-align: center;">
-                                        No tiene ningun producto agregado al carrito
-                                    </p>
-                                @endif
+                                                        <a 
+
+                                                            class="btn btn-warning btn-update-item"
+                                                            data-href="{{ route('cart.update', [$item->product->slug]) }}"
+                                                            data-id ="{{ $item->product->id }}"
+                                                            style="margin-right: -30px; margin-left: 5px;" 
+                                                        >
+                                                            <i class="fa fa-refresh"></i>
+                                                        </a>
+
+                                                    </td>
+
+                                                    <td>{{ number_format($item->product->price * $item->quantity,2) }} Bs</td>
+                                                
+                                                    <td>
+                                                        <a href="{{ route('cart.delete', [$item->product->slug]) }}" class="btn btn-danger">
+                                                            <i class="fa fa-remove"></i>
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                    </tbody>
+                                </table>
                         </div><!-- /.col -->
                     </div><!-- /.row -->
 
+                    <hr>
                     <div class="row">
-                        <!-- accepted payments column -->
 
-                        <div class="col-xs-6">
-                            <p class="lead">Payment Methods:</p><img src=
-                            "images/visa.png" alt="Visa" /> <img src=
-                            "images/mastercard.png" alt="Mastercard" /> <img src=
-                            "images/american-express.png" alt="American Express" />
-                            <img src="images/paypal2.png" alt="Paypal" />
-
-                            <p class="text-muted well well-sm no-shadow" style=
-                            "margin-top: 10px;">Etsy doostang zoodles disqus groupon
-                            greplin oooj voxy zoodles, weebly ning heekya handango imeem
-                            plugg dopplr jibjab, movity jajah plickers sifteo edmodo
-                            ifttt zimbra.</p>
-                        </div><!-- /.col -->
-
-                        <div class="col-xs-6">
-                            <p class="lead">Amount Due 2/22/2014</p>
+                        <div class="col-xs-12">
+                            <p class="lead">Detalles de la Compra</p>
 
                             <div class="table-responsive">
                                 <table class="table">
@@ -146,7 +115,6 @@
                                                     $subtotal += $item->product->price * $item->quantity;
                                                     $porciento = $item->product->street_percentage;
                                                     $porcentaje = $subtotal * number_format($porciento)/100;
-                                                    $total = number_format($subtotal + $porcentaje,2);
 
                                                 @endphp
                                             @endforeach
@@ -155,35 +123,43 @@
                                                 <th style="width:50%">Subtotal:</th>
                                             
 
-                                            <td>{{ number_format($subtotal,2) }}</td>
+                                            <td>{{ number_format($subtotal,2) }} Bs</td>
                                             </tr>
 
                                             <tr>
-                                                <th>Porcentaje {{ number_format($porciento) }}%</th>
+                                                <th>Porcentaje Calle: {{ number_format($porciento) }}%</th>
 
-                                                <td>{{ number_format($porcentaje,2) }}</td>
+                                                <td>{{ number_format($porcentaje,2) }} Bs</td>
                                             </tr>
 
                                             <tr>
                                                 <th>Total:</th>
 
-                                                <td>{{ $total }}</td>
+                                                <td>{{ number_format($total + $porcentaje) }} Bs</td>
                                             </tr>
                                         
                                     </tbody>
                                 </table>
+                            @endif
+
                             </div>
                         </div><!-- /.col -->
+                            @if(count($cart)<1)
+                                <h2 style="text-align: center;">
+                                    No tiene ningun producto agregado al carrito
+                                </h2> 
+                            @endif
                     </div><!-- /.row -->
-                    <!-- this row will not appear when printing -->
 
-                    <div class="row no-print">
-                        <div class="col-xs-12">
-                            <button class="btn btn-default" onclick=
-                            "window.print();">Print</button> <button class=
-                            "btn btn-success pull-right"> Submit Payment</button>
-                            <button class="btn btn-primary pull-right" style=
-                            "margin-right: 5px;"> Generate PDF</button>
+                    <hr>
+                    <div class="row text-center">
+                        <div class="col-12">
+                            @if(count($cart)>0)
+                                <button class="btn btn-success pull-right"> Enviar Solicitud de Compra</button>
+                            @endif
+                            <a href="{{ route('catalogue.index')}}">
+                                <button class="btn btn-primary pull-right" style="margin-right: 5px;"> Volver</button>
+                            </a>
                         </div>
                     </div>
                 </div>
