a09ae831e1a6d57bd042bcf17196b401275a5021
Boanerget <antonyprogramacion96@gmail.com>
2017-12-05 09:51:21 -0400

    [MOD] Modificaciones esteticas menores.

--- resources/views/catalogue/index.blade.php
+++ resources/views/catalogue/index.blade.php
@@ -1,10 +1,8 @@
 @extends('layouts.master_user')
 
-
 @section('title')
 Catalogo
 @stop
-
 
 @section('content')
 
@@ -14,17 +12,6 @@ Catalogo
         <div class="title_left">
             <h3>Catalogo de Productos</h3>
         </div>
-
-        <div class="title_right">
-            <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search for...">
-                    <span class="input-group-btn">
-                        <button class="btn btn-default" type="button"></button>
-                    </span>
-                </div>
-            </div>
-        </div>
     </div>
     <div class="clearfix"></div>
 
@@ -32,37 +19,34 @@ Catalogo
         <div class="col-md-12">
             <div class="x_panel">
                 <div class="x_content">
-
                     <div class="row">
-
                         <div class="col-md-12 col-sm-12 col-xs-12" style="text-align:center;">
                             {{ $inventory->links() }}
                         </div>
                         <div class="clearfix"></div>
 
                         @foreach($inventory as $product)
-                        <div class="col-md-3">
-                            <p>Producto: {{ ucwords($product->product->name) }}  </p>
-                            <div class="thumbnail" style="height: 5%; padding: 0;">
-                                <div class="image view view-first">
-                                    <img style="width: 80%; height: 100%; margin: auto;" src="{{ asset('images/products/').'/'.$product->product->image }}" alt="image" />
-                                    <div class="mask">
-                                        <p>Producto {{ ucwords($product->product->name) }}</p>
-                                        <div class="tools tools-bottom">
-                                            <a href="{{ route('catalogue.show',[$product->slug]) }}"><i class="fa fa-eye"></i></a>
-                                            <a href="{{ route('cart.add',[$product->slug]) }}"><i class="fa fa-shopping-cart cart" style="margin-top:0; margin-bottom: 3px;"></i></a>
+                            <div class="col-md-3">
+                                <p>Producto: {{ ucwords($product->product->name) }}</p>
+                                <div class="thumbnail" style="height: 5%; padding: 0;">
+                                    <div class="image view view-first">
+                                        <img style="width: 80%; height: 100%; margin: auto;" src="{{ asset('images/products/').'/'.$product->product->image }}" alt="image" />
+                                        <div class="mask">
+                                            <p>Producto {{ ucwords($product->product->name) }}</p>
+                                            <div class="tools tools-bottom">
+                                                <a href="{{ route('catalogue.show',[$product->slug]) }}"><i class="fa fa-eye"></i></a>
+                                                <a href="{{ route('cart.add',[$product->slug]) }}"><i class="fa fa-shopping-cart cart" style="margin-top:0; margin-bottom: 3px;"></i></a>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="product_price" style="margin: 0; padding: 0;">
+                                    <div class="product_price" style="margin: 0; padding: 0;">
+                                        <h4>Marca: {{$product->product->brand}}</h4>
+                                        <h4>Cantidad: {{$product->quantity_available}}{{$product->unit->abbreviation}}  </h4>
+                                        <h4 class="price">Precio: {{ number_format($product->product->price,2) }}Bs</h4>
+                                    </div>
 
-                                    <h4>Marca: {{$product->product->brand}}</h4>
-                                    <h4>Cantidad: {{$product->quantity_available}}{{$product->unit->abbreviation}}  </h4>
-                                    <h4 class="price">Precio: {{ number_format($product->product->price,2) }}Bs</h4>
                                 </div>
-
                             </div>
-                        </div>
 
                         @endforeach
                         
