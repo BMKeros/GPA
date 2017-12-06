03b1411c4c7758cc851f76dda8572d1a78aad031
AjxGnx <alirio1925@gmail.com>
2017-12-05 23:59:41 -0400

    [ADD] se agrego al config correo que envia mensajes

--- config/mail.php
+++ config/mail.php
@@ -55,10 +55,7 @@ return [
     |
     */
 
-    'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
-    ],
+    'from' => ['address' => 'alirio1925@gmail.com', 'name' => 'Test'],
 
     /*
     |--------------------------------------------------------------------------
