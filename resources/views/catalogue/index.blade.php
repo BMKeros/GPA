cbfb14acf83fce0961123d676fd2a5229eb96055
Boanerget <antonyprogramacion96@gmail.com>
2017-12-06 13:47:26 -0400

    [FIXED] Corregido el error de no mostrar los productos en linea.

--- resources/views/catalogue/index.blade.php
+++ resources/views/catalogue/index.blade.php
@@ -25,39 +25,39 @@ Catalogo
                         </div>
                         <div class="clearfix"></div>
 
-                        @if(count($inventory)<1)
-                            <h2 style="text-align: center;">
-                                No hay ningún producto en el catalogo.
-                            </h2>
-                        @else
-                            @foreach($inventory as $product)
+                    @if(count($inventory)<1)
+                        <h2 style="text-align: center;">
+                            No hay ningún producto en el catalogo.
+                        </h2>
+                    @else
+                        @foreach($inventory as $product)
 
-                                <div class="col-md-3">
-                                    <p>Producto: {{ ucwords($product->product->name) }}</p>
-                                    <div class="thumbnail" style="height: 5%; padding: 0;">
-                                        <div class="image view view-first">
-                                            <img style="width: 80%; height: 100%; margin: auto;" src="{{ asset('images/products/').'/'.$product->product->image }}" alt="image" />
-                                            <div class="mask">
-                                                <p>Producto {{ ucwords($product->product->name) }}</p>
-                                                <div class="tools tools-bottom">
-                                                    <a href="{{ route('catalogue.show',[$product->slug]) }}"><i class="fa fa-eye"></i></a>
-                                                    <a href="{{ route('cart.add',[$product->slug]) }}"><i class="fa fa-shopping-cart cart" style="margin-top:0; margin-bottom: 3px;"></i></a>
-                                                </div>
+                            <div class="col-md-3">
+                                <p>Producto: {{ ucwords($product->product->name) }}</p>
+                                <div class="thumbnail" style="height: 5%; padding: 0;">
+                                    <div class="image view view-first">
+                                        <img style="width: 80%; height: 100%; margin: auto;" src="{{ asset('images/products/').'/'.$product->product->image }}" alt="image" />
+                                        <div class="mask">
+                                            <p>Producto {{ ucwords($product->product->name) }}</p>
+                                            <div class="tools tools-bottom">
+                                                <a href="{{ route('catalogue.show',[$product->slug]) }}"><i class="fa fa-eye"></i></a>
+                                                <a href="{{ route('cart.add',[$product->slug]) }}"><i class="fa fa-shopping-cart cart" style="margin-top:0; margin-bottom: 3px;"></i></a>
                                             </div>
                                         </div>
-                                        <div class="product_price" style="margin: 0; padding: 0;">
-                                            <h4>Marca: {{$product->product->brand}}</h4>
-                                            <h4>Cantidad: {{$product->quantity_available}}{{$product->unit->abbreviation}}  </h4>
-                                            <h4 class="price">Precio: {{ number_format($product->product->price,2) }}Bs</h4>
-                                        </div>
-
                                     </div>
+                                    <div class="product_price" style="margin: 0; padding: 0;">
+                                        <h4>Marca: {{$product->product->brand}}</h4>
+                                        <h4>Cantidad: {{$product->quantity_available}}{{$product->unit->abbreviation}}  </h4>
+                                        <h4 class="price">Precio: {{ number_format($product->product->price,2) }}Bs</h4>
+                                    </div>
+
                                 </div>
-                                <div class="col-md-12 col-sm-12 col-xs-12" style="text-align:center;">
-                                    {{ $inventory->links() }}
-                                </div>
-                            @endforeach
-                        @endif
+                            </div>
+                        @endforeach
+                            <div class="col-md-12 col-sm-12 col-xs-12" style="text-align:center;">
+                                {{ $inventory->links() }}
+                            </div>
+                    @endif
                         
                        
                     </div>
