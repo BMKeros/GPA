e33eaab72a6b1ea28a43027009b2d359f8c3d1d7
Jesus Navas <jesusnavas2210@gmail.com>
2017-12-07 03:01:40 -0400

    [MOD] se agregaron las migraciones para los pedidos y solicitudes

---
+++ database/migrations/2017_12_07_053709_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('request_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+            $table->integer('status_id')->unsigned();
+            $table->integer('quantity');
+            $table->decimal('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
