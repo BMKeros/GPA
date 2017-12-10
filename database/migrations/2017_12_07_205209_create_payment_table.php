bf4f7a5db8a06f14f78ad56dc31bcaecf825b33e
noel <toyonoel@gmail.com>
2017-12-09 22:11:15 -0400

    [ADD] se agrego la migraciones para la tabla de abonos

---
+++ database/migrations/2017_12_07_205209_create_payment_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('status_id')->unsigned();
+            $table->foreign('status_id')->references('id')->on('status');
+            $table->integer('request_id')->unsigned();
+            $table->foreign('request_id')->references('id')->on('purchase_requests');
+
+            $table->integer('payment_method_id')->unsigned();
+            $table->foreign('payment_method_id')->references('id')->on('payment_methods');
+
+            $table->string('image')->nullable();
+            $table->string('description')->nullable();
+            $table->decimal('quantity');
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment');
+
+    }
+}
