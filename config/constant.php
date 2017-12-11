6c6515e6fe587da2c1c57bf7713df9470acc2f5e
Israel Lugo <hostelixisrael@gmail.com>
2017-12-11 13:46:54 -0400

    [ADD] agregado el archivo constantes

---
+++ config/constant.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'status' => [
+        "DISPONIBLE" => 1,
+        "NO_DISPONIBLE" => 2,
+        "PENDIENTE" => 3,
+        "ACEPTADO" => 4,
+        "RECHAZADO" => 5,
+        "PAGADO" => 6
+    ]
+];
