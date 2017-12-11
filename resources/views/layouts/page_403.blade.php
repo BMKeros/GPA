4e5dd017847e8cde5203025f9b6f7337ad590b0d
Boanerget <antonyprogramacion96@gmail.com>
2017-12-11 13:14:49 -0400

    [MOD] Cambiada información del error.

--- resources/views/layouts/page_403.blade.php
+++ resources/views/layouts/page_403.blade.php
@@ -9,7 +9,7 @@
 	    <div class="col-middle">
 	        <div class="text-center text-center">
 	            <h1 class="error-number">403</h1>
-	            <h2>Error de autenticación</h2>
+	            <h2>Acceso denegado</h2>
 	            <p>Usted no tiene los permisos requeridos para acceder al recurso que esta solicitando</a>
 	            </p>
 	        </div>
