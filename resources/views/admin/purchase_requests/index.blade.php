440bc31c3e918c2faf015c29465a4203d262413a
Boanerget <antonyprogramacion96@gmail.com>
2017-12-26 11:10:20 -0400

    [MOD] Mejorado el aspecto de los botones.

--- resources/views/admin/purchase_requests/index.blade.php
+++ resources/views/admin/purchase_requests/index.blade.php
@@ -33,34 +33,45 @@
 
                                 <tr>
                                     <td>{{ $purchase->user->name }}</td>
-                                    <td>{{ $purchase->status->name }}</td>
+
+                                    <td style="text-align: center; width: 30%;">
+                                        @if ($purchase->status->id == 3)
+                                            <span class="btn btn-primary btn-xs" >Pendiente</span>
+                                        @elseif($purchase->status->id == 4)
+                                            <span class="btn btn-success btn-xs">Aceptado</span>
+                                        @else
+                                            <span class="btn btn-danger btn-xs">Rechazado</span>      
+                                        @endif
+                                    </td>
+
                                     <td>{{ $purchase->description }}</td>
                                     <td style="width: 30%;">
 
-                                        <a style="display:inline-block;float:left;" href="{{ route('admin-order.show',$purchase['id']) }}" class="btn btn-info btn-xs"><i class="fa fa-pencil"></i> Ver </a>
+                                        <a href="{{route('admin-order.show',$purchase['id'])}}" class="btn btn-info btn-md" data-toggle="tooltip" title="Ver!"><span class="glyphicon glyphicon-eye-open"></span></a>
                                         @if($purchase->status_id == '3')
                                     
-                                            <form style="display:inline-block;float:left;" action="{{route('purchase-requests.update', [$purchase->id])}}" method="POST">
+                                            <form style="display:inline-block;float:left;" action="{{route('admin-purchase-requests.update', [$purchase->id])}}" method="POST">
                                                 {{csrf_field()}}
 
                                                 <input name="_method" type="hidden" value="PATCH">
                                                 
                                                 <input name="accion" type="hidden" value="aceptar">
 
-                                                <button type="submit" class="btn btn-success btn-xs"><i class="fa fa-thumbs-up"></i> Aceptar
+                                                <button type="submit" class="btn btn-success btn-md" data-toggle="tooltip" title="Aceptar!"><span class="glyphicon glyphicon-ok"></span>
                                                 </button>
 
                                             </form>
 
-                                            <form style="display:inline-block;float:left;" action="{{route('purchase-requests.update', [$purchase->id])}}" method="POST">
+                                            <form style="display:inline-block;float:left;" action="{{route('admin-purchase-requests.update', [$purchase->id])}}" method="POST">
                                                 {{csrf_field()}}
 
                                                 <input name="_method" type="hidden" value="PATCH">
 
                                                 <input name="accion" type="hidden" value="rechazar">
                                                 
-                                                <button type="submit" class="btn btn-danger btn-xs"><i class="fa fa-thumbs-down"></i> Rechazar
+                                                <button type="submit" class="btn btn-danger btn-md" data-toggle="tooltip" title="Rechazar!"><span class="glyphicon glyphicon-remove"></span>
                                                 </button>
+
 
                                             </form>
 
